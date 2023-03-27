7f7cff7dcc05f4a48a40b9e7bd2f5cf333fcc0cb
Nguyen Ho <honguyen130120021@gmail.com>
2023-03-27 12:27:09 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import Navigation from './navigation';
+
+export default function App () {
+  return (
+    <Navigation />
+  );
+};
+
+const styles = StyleSheet.create({});
---
+++ navigation/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// import Screens
+import Welcome from "../screens/Welcome";
+import Rewards from "../screens/Rewards";
+import Trip from "../screens/Trip";
+
+const Stack = createNativeStackNavigator();
+
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerBackTitleVisible:false}}>
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Rewards" component={Rewards} />
+        <Stack.Screen name="Trip" component={Trip} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Navigation;
